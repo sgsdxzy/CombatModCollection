19d3acf665147788c95d7f0f7fd14c8cb4a4be09
sgsdxzy <sgsdxzy@gmail.com>
2020-05-11 19:11:25 +0800

    Implemented Item evalutaion

--- src/SendAllTroops/TroopTemplate.cs
+++ src/SendAllTroops/TroopTemplate.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.ObjectSystem;
 
 namespace CombatModCollection
@@ -18,6 +19,8 @@ namespace CombatModCollection
         public readonly float Atheletics;
         public readonly float ArmorPoints;
         public readonly float Strength;
+
+        static System.IO.StreamWriter templateFile = new System.IO.StreamWriter(@"D:\TroopTemplates.txt");
 
         public static TroopTemplate GetTroopTemplate(CharacterObject troop)
         {
@@ -45,7 +48,11 @@ namespace CombatModCollection
             Atheletics = troop.GetSkillValue(DefaultSkills.Athletics) / (totalWeight + 3f) * 0.01f;
             ArmorPoints = (float)(0.3 + Math.Pow(armorSum, 0.75) / 40.0);
 
-            float bestShield = 0.0f;
+            templateFile.WriteLine(troop.Name);
+            templateFile.WriteLine("Atheletics: " + Atheletics);
+            templateFile.WriteLine("ArmorPoints: " + ArmorPoints);
+
+            float bestShield = 0;
             for (EquipmentIndex index1 = EquipmentIndex.WeaponItemBeginSlot; index1 < EquipmentIndex.NumAllWeaponSlots; ++index1)
             {
                 EquipmentElement equipmentElement1 = equipment[index1];
@@ -54,55 +61,66 @@ namespace CombatModCollection
                     if (equipmentElement1.Item.PrimaryWeapon.IsShield)
                     {
                         float proficiency = equipmentElement1.Item.RelevantSkill == null ? 1f : 0.3f + troop.GetSkillValue(equipmentElement1.Item.RelevantSkill) / 300.0f * 0.7f;
-                        float strength = GetShieldStrength(equipmentElement1.Item) * proficiency;
-                        if (strength > bestShield)
+                        float strength = GetShieldStrength(equipmentElement1.Item);
+                        if (strength * proficiency > bestShield)
                         {
-                            bestShield = strength;
-                            Shield = new Item { Strength = strength };
+                            bestShield = strength * proficiency;
+                            Shield = new Item { Strength = strength * proficiency };
                         }
+
+                        templateFile.WriteLine(String.Format("Shield: {0} {1:G3}*{2:G3}={3:G3}",
+                            equipmentElement1.Item.Name, strength, proficiency, strength * proficiency));
                     }
                     else if (equipmentElement1.Item.PrimaryWeapon.IsMeleeWeapon)
                     {
-                        float proficiency = equipmentElement1.Item.RelevantSkill == null ? 1f : 0.3f + troop.GetSkillValue(equipmentElement1.Item.RelevantSkill) / 300.0f * 0.7f;
-                        float strength = GetMeleeWeaponStrength(equipmentElement1.Item) * proficiency;
-                        Weapon weapon = new Weapon
+                        templateFile.WriteLine("Melee Weapon: " + equipmentElement1.Item.Name);
+                        foreach (var weaponData in equipmentElement1.Item.Weapons)
                         {
-                            Range = 0,
-                            HasLimitedAmmo = false
-                        };
-                        switch (equipmentElement1.Item.PrimaryWeapon.WeaponClass)
-                        {
-                            case WeaponClass.Dagger:
-                            case WeaponClass.OneHandedSword:
-                            case WeaponClass.OneHandedAxe:
-                            case WeaponClass.Mace:
-                                weapon.IsTwohanded = false;
-                                weapon.Attack.Melee = strength;
-                                break;
-                            case WeaponClass.TwoHandedSword:
-                            case WeaponClass.TwoHandedAxe:
-                            case WeaponClass.TwoHandedMace:
-                                weapon.IsTwohanded = true;
-                                weapon.Attack.Melee = strength * 1.5f;
-                                break;
-                            case WeaponClass.OneHandedPolearm:
-                            case WeaponClass.LowGripPolearm:
-                                weapon.IsTwohanded = false;
-                                weapon.Attack.Polearm = strength;
-                                break;
-                            case WeaponClass.TwoHandedPolearm:
-                                weapon.IsTwohanded = true;
-                                weapon.Attack.Polearm = strength * 1.5f;
-                                break;
+                            float proficiency = weaponData.RelevantSkill == null ? 1f : 0.3f + troop.GetSkillValue(weaponData.RelevantSkill) / 300.0f * 0.7f;
+                            float strength = GetMeleeWeaponStrength(weaponData);
+                            templateFile.WriteLine(String.Format("Mode: {0:G3}*{1:G3}={2:G3}",
+                                strength, proficiency, strength * proficiency));
+                            Weapon weapon = new Weapon
+                            {
+                                Range = 0,
+                                HasLimitedAmmo = false
+                            };
+                            switch (weaponData.WeaponClass)
+                            {
+                                case WeaponClass.Dagger:
+                                case WeaponClass.OneHandedSword:
+                                case WeaponClass.OneHandedAxe:
+                                case WeaponClass.Mace:
+                                    weapon.IsTwohanded = false;
+                                    weapon.Attack.Melee = strength * proficiency;
+                                    break;
+                                case WeaponClass.TwoHandedSword:
+                                case WeaponClass.TwoHandedAxe:
+                                case WeaponClass.TwoHandedMace:
+                                    weapon.IsTwohanded = true;
+                                    weapon.Attack.Melee = strength * proficiency * 1.5f;
+                                    break;
+                                case WeaponClass.OneHandedPolearm:
+                                case WeaponClass.LowGripPolearm:
+                                    weapon.IsTwohanded = false;
+                                    weapon.Attack.Polearm = strength * proficiency;
+                                    break;
+                                case WeaponClass.TwoHandedPolearm:
+                                    weapon.IsTwohanded = true;
+                                    weapon.Attack.Polearm = strength * proficiency * 1.5f;
+                                    break;
+                            }
+                            Weapons.Add(weapon);
                         }
-                        Weapons.Add(weapon);
                     }
                     else if (equipmentElement1.Item.PrimaryWeapon.IsRangedWeapon)
                     {
                         if (equipmentElement1.Item.Type == ItemObject.ItemTypeEnum.Thrown)
                         {
                             float proficiency = equipmentElement1.Item.RelevantSkill == null ? 1f : 0.3f + troop.GetSkillValue(equipmentElement1.Item.RelevantSkill) / 300.0f * 0.7f;
-                            float strength = GetThrownWeaponStrength(equipmentElement1.Item) * proficiency;
+                            float strength = GetThrownWeaponStrength(equipmentElement1.Item.PrimaryWeapon);
+                            templateFile.WriteLine(String.Format("Thrown: {0} {1:G3}*{2:G3}={3:G3}",
+                                equipmentElement1.Item.Name, strength, proficiency, strength * proficiency));
                             Weapon weapon = new Weapon
                             {
                                 Range = 1,
@@ -110,13 +128,13 @@ namespace CombatModCollection
                                 HasLimitedAmmo = true,
                                 RemainingAmmo = (int)Math.Round(equipmentElement1.Item.PrimaryWeapon.MaxDataValue * AmmoMultiplier)
                             };
-                            weapon.Attack.Missile = strength * 1.5f;
+                            weapon.Attack.Missile = strength * proficiency * 1.5f;
                             Weapons.Add(weapon);
                         }
                         else
                         {
                             float proficiency = equipmentElement1.Item.RelevantSkill == null ? 1f : 0.3f + troop.GetSkillValue(equipmentElement1.Item.RelevantSkill) / 300.0f * 0.7f;
-                            float strength = GetRangedWeaponStrength(equipmentElement1.Item) * proficiency;
+                            float strength = GetRangedWeaponStrength(equipmentElement1.Item);
                             float numAmmo = 0;
                             float ammoStrength = 0;
                             for (EquipmentIndex index2 = EquipmentIndex.WeaponItemBeginSlot; index2 < EquipmentIndex.NumAllWeaponSlots; ++index2)
@@ -142,67 +160,113 @@ namespace CombatModCollection
                                     HasLimitedAmmo = true,
                                     RemainingAmmo = (int)Math.Round(numAmmo * AmmoMultiplier)
                                 };
-                                weapon.Attack.Missile = (strength + ammoStrength) * 1.5f;
+                                weapon.Attack.Missile = (strength + ammoStrength) * proficiency * 1.5f;
                                 Weapons.Add(weapon);
-                                // FavorateWeapon = weapon;
+                                templateFile.WriteLine(String.Format("Ranged: {0} ({1:G3}+{2:G3})*{3:G3}={4:G3}",
+                                    equipmentElement1.Item.Name, strength, ammoStrength, proficiency, (strength + ammoStrength) * proficiency));
                             }
                         }
                     }
                 }
             }
 
-            float bestHorse = Atheletics;
+            float bestMovementBonus = Atheletics;
             if (equipment.Horse.Item != null)
             {
                 float proficiency = equipment.Horse.Item.RelevantSkill == null ? 1f : 0.3f + troop.GetSkillValue(equipment.Horse.Item.RelevantSkill) / 300.0f * 0.7f;
-                float strength = GetHorseStrength(equipment.Horse.Item, equipment[EquipmentIndex.HorseHarness].Item) * proficiency;
-                Horse = new Item { Strength = strength };
-                bestHorse = strength;
+                float strength = GetHorseStrength(equipment.Horse.Item, equipment[EquipmentIndex.HorseHarness].Item);
+                Horse = new Item { Strength = strength * proficiency };
+                bestMovementBonus = strength * proficiency;
+
+                templateFile.WriteLine(String.Format("Horse: {0:G3}*{1:G3}={2:G3}",
+                    strength, proficiency, strength * proficiency));
             }
 
-            float bestWeapon = 0.0f;
-            bool twoHanded = false;
+            float bestWeaponStrength = 0.0f;
+            bool isBestWeaponTwoHanded = false;
             foreach (var weapon in Weapons)
             {
-                float damage = weapon.Attack.Sum();
-                if (damage > bestWeapon)
+                float weaponStrength = weapon.Attack.Sum();
+                if (weaponStrength > bestWeaponStrength)
                 {
-                    bestWeapon = damage;
-                    twoHanded = weapon.IsTwohanded;
+                    bestWeaponStrength = weaponStrength;
+                    isBestWeaponTwoHanded = weapon.IsTwohanded;
                 }
             }
 
-            Strength = (ArmorPoints + (twoHanded ? 0 : bestShield)) * bestWeapon * (1 + bestHorse);
+            Strength = (ArmorPoints + (isBestWeaponTwoHanded ? 0 : bestShield)) * bestWeaponStrength * (1 + bestMovementBonus) * troop.MaxHitPoints() / 100;
+            templateFile.WriteLine("Strength: " + Strength);
+            templateFile.WriteLine();
+            templateFile.Flush();
         }
 
-        private float GetMeleeWeaponStrength(ItemObject item)
+        private static float GetFactor(DamageTypes swingDamageType)
         {
-            return (int)item.Tier * 0.2f + 0.8f;
+            if (swingDamageType == DamageTypes.Blunt)
+                return 1.3f;
+            return swingDamageType != DamageTypes.Pierce ? 1f : 1.15f;
+        }
+
+        private float GetMeleeWeaponStrength(WeaponComponentData weaponData)
+        {
+            float num1 = Math.Max((float)weaponData.ThrustDamage * GetFactor(weaponData.ThrustDamageType)
+                * MathF.Pow((float)weaponData.ThrustSpeed * 0.01f, 1.5f),
+                (float)weaponData.SwingDamage * GetFactor(weaponData.SwingDamageType)
+                * MathF.Pow((float)weaponData.SwingSpeed * 0.01f, 1.5f) * 1.1f);
+            float num2 = (float)weaponData.WeaponLength * 0.01f;
+            float tierf = (float)(0.06 * ((double)num1 * (1.0 + (double)num2)) - 3.5);
+
+            return tierf * 0.2f + 0.8f;
+        }
+
+        private float GetThrownWeaponStrength(WeaponComponentData weaponData)
+        {
+            float num1 = Math.Max((float)weaponData.ThrustDamage * GetFactor(weaponData.ThrustDamageType)
+                * MathF.Pow((float)weaponData.ThrustSpeed * 0.01f, 1.5f),
+                (float)weaponData.SwingDamage * GetFactor(weaponData.SwingDamageType)
+                * MathF.Pow((float)weaponData.SwingSpeed * 0.01f, 1.5f) * 1.1f);
+            if (weaponData.WeaponClass == WeaponClass.ThrowingKnife || weaponData.WeaponClass == WeaponClass.Javelin)
+                num1 *= 1.2f;
+            float num2 = (float)weaponData.WeaponLength * 0.01f;
+            float tierf = (float)(0.06 * ((double)num1 * (1.0 + (double)num2)) - 3.5);
+
+            return tierf * 0.2f + 0.8f;
         }
 
         private float GetRangedWeaponStrength(ItemObject item)
         {
-            return ((int)item.Tier * 0.2f + 0.8f) * 0.9f;
+            WeaponComponentData weaponData = item.PrimaryWeapon;
+            double num1;
+            switch (item.ItemType)
+            {
+                case ItemObject.ItemTypeEnum.Crossbow:
+                    num1 = 0.7;
+                    break;
+                case ItemObject.ItemTypeEnum.Musket:
+                    num1 = 0.5;
+                    break;
+                default:
+                    num1 = 1.0;
+                    break;
+            }
+            double num2 = (double)weaponData.ThrustDamage * 0.2 + (double)weaponData.ThrustSpeed * 0.02 + (double)weaponData.Accuracy * 0.02;
+            float tierf = (float)(num1 * num2 - 11.0);
+
+            return (tierf * 0.2f + 0.8f) * 0.9f;
         }
 
         private float GetRangedAmmoStrength(ItemObject item)
         {
-            return ((int)item.Tier * 0.2f + 0.8f) * 0.1f;
-        }
-
-        private float GetThrownWeaponStrength(ItemObject item)
-        {
-            WeaponComponentData nweapon = item.WeaponComponent.PrimaryWeapon;
-            if (nweapon.WeaponClass == WeaponClass.Stone)
-            {
-                return 0.4f;
-            }
-            return (int)item.Tier * 0.2f + 0.8f;
+            var weaponData = item.PrimaryWeapon;
+            float tier = (float)weaponData.MissileDamage;
+            return (tier * 0.2f + 0.8f) * 0.1f;
         }
 
         private float GetShieldStrength(ItemObject item)
         {
-            return ((int)item.Tier * 0.2f + 0.8f) * 0.25f;
+            var weaponData = item.PrimaryWeapon;
+            float tierf = (float)((weaponData.MaxDataValue + 3.0 * weaponData.BodyArmor + weaponData.ThrustSpeed) / (6.0 + weaponData.Item.Weight) * 0.13 - 3.0);
+            return (tierf * 0.2f + 0.8f) * 0.25f;
         }
 
         private float GetHorseStrength(ItemObject itemHorse, ItemObject itemHarness)
@@ -213,7 +277,12 @@ namespace CombatModCollection
                 harnessStrength = (float)Math.Pow(itemHarness.ArmorComponent.BodyArmor, 0.4);
             }
 
-            return itemHorse.Tierf * harnessStrength * 0.05f;
+            var horseComponent = itemHorse.HorseComponent;
+            float horseTierf = (float)((horseComponent.IsPackAnimal ? 25.0 :
+                0.6 * (double)horseComponent.Maneuver + (double)horseComponent.Speed
+                + 1.5 * (double)horseComponent.ChargeDamage + 0.1 * (double)horseComponent.HitPoints) / 13.0 - 8.0);
+
+            return horseTierf * harnessStrength * 0.05f;
         }
     }
 }
