da620fd4542d93b5ec08ffe1b52d539c5c4a2cbb
sgsdxzy <sgsdxzy@gmail.com>
2020-05-15 00:52:13 +0800

    Cleanups

--- src/SubModule.cs
+++ src/SubModule.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-using System.IO;
-using System.Reflection;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
@@ -9,30 +7,7 @@ namespace CombatModCollection
 {
     public class SubModule : MBSubModuleBase
     {
-        //public static Settings Settings { get; private set; }
         private bool Patched = false;
-
-        private static void LoadSettings()
-        {
-            string configPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "settings.json");
-            try
-            {
-                //Settings.Instance = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(configPath));
-            }
-            catch
-            { }
-        }
-
-
-        protected override void OnSubModuleLoad()
-        {
-            base.OnSubModuleLoad();
-
-            //SubModule.LoadSettings();
-            //var harmony = new Harmony("mod.bannerlord.lightcombat");
-            //harmony.PatchAll();
-            // bool a = Settings.Instance.Battle_PowerThrust;
-        }
 
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
@@ -44,14 +19,12 @@ namespace CombatModCollection
             }
         }
 
-
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             base.OnGameStart(game, gameStarterObject);
             AddModels(gameStarterObject as CampaignGameStarter);
             AddBehaviors(gameStarterObject as CampaignGameStarter);
         }
-
 
         private void AddModels(CampaignGameStarter gameStarter)
         {
@@ -60,7 +33,6 @@ namespace CombatModCollection
                 gameStarter?.AddModel(new LightBanditDensityModel());
             }
         }
-
 
         private void AddBehaviors(CampaignGameStarter gameStarter)
         {
