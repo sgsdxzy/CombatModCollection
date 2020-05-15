aeff931459ac39710b092d007117e56bc2085816
sgsdxzy <sgsdxzy@gmail.com>
2020-05-15 20:50:30 +0800

    Splited ManagedParametersPatcher

---
+++ src/PowerThrust/ManagedParametersPatches/InitializePatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+
+namespace CombatModCollection.PowerThrust.ManagedParametersPatches
+{
+    [HarmonyPatch(typeof(ManagedParameters), "Initialize")]
+    public class InitializePatch
+    {
+        public static void Postfix()
+        {
+            ManagedParameters.SetParameter(ManagedParametersEnum.NonTipThrustHitDamageMultiplier,
+                Settings.Instance.Battle_PowerThrust_NonTipThrustHitDamageMultiplier);
+            ManagedParameters.SetParameter(ManagedParametersEnum.ThrustHitWithArmDamageMultiplier,
+                Settings.Instance.Battle_PowerThrust_ThrustHitWithArmDamageMultiplier);
+            ManagedParameters.SetParameter(ManagedParametersEnum.ThrustCombatSpeedGraphZeroProgressValue, 1.0f);
+        }
+
+        public static bool Prepare()
+        {
+            return Settings.Instance.Battle_PowerThrust;
+        }
+    }
+}
---
+++ src/RealisticBallistics/ManagedParametersPatches/InitializePatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+
+namespace CombatModCollection.RealisticBallistics.ManagedParametersPatches
+{
+    [HarmonyPatch(typeof(ManagedParameters), "Initialize")]
+    public class InitializePatch
+    {
+        public static void Postfix()
+        {
+            ManagedParameters.SetParameter(ManagedParametersEnum.AirFrictionArrow,
+                 Settings.Instance.Battle_RealisticBallistics_AirFrictionArrow);
+            ManagedParameters.SetParameter(ManagedParametersEnum.AirFrictionJavelin,
+                Settings.Instance.Battle_RealisticBallistics_AirFrictionJavelin);
+            ManagedParameters.SetParameter(ManagedParametersEnum.AirFrictionAxe,
+                 Settings.Instance.Battle_RealisticBallistics_AirFrictionAxe);
+            ManagedParameters.SetParameter(ManagedParametersEnum.AirFrictionKnife,
+                 Settings.Instance.Battle_RealisticBallistics_AirFrictionKnife);
+        }
+
+        public static bool Prepare()
+        {
+            return Settings.Instance.Battle_RealisticBallistics;
+        }
+    }
+}
